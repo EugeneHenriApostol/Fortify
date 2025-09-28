87073e9a14040b624dc9b5f80d6966c2f3617878
EugeneHenriApostol <eugenehenriapostol@gmail.com>
2025-09-28 21:56:42 +0800

    added landing page

--- fortify-frontend/src/App.jsx
+++ fortify-frontend/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./Components/Login";
+import LandingPage from "./Components/LandingPage";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<LandingPage />} />
       </Routes>
     </Router>
   );
